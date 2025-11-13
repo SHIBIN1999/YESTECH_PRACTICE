dce1c5a0e2e3b0ebbf1998de709d3aa28424f0d7
shibin <shibinshahair7@gmail.com>
2025-11-13 13:12:43 +0530

    tag add fruits item list

--- firstproject/subapp/views.py
+++ firstproject/subapp/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    n={'s':[1,2,3,4,5]}
+    n={'s':['orange','apple']}
     return render(request,'index.html',n)
