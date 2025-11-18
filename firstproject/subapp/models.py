1dfca443aa17f7b8cb52a40ed4892fa48a9744fa
shibin <shibinshahair7@gmail.com>
2025-11-18 13:53:01 +0530

    imagefield_created

--- firstproject/subapp/models.py
+++ firstproject/subapp/models.py
@@ -4,3 +4,10 @@ from django.db import models
 class department(models.Model):
     dep_name=models.CharField(max_length=100)
     dep_description=models.TextField()
+
+
+class doctors(models.Model):
+    doc_name=models.CharField(max_length=100)
+    doc_spec=models.CharField(max_length=100)
+    dep_name=models.ForeignKey(department,on_delete=models.CASCADE)
+    doc_image=models.ImageField(upload_to='doctors')
